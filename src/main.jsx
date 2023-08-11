abd0c518cfb2b8447fb4640c2e905a34da3e6823
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-08-10 22:36:24 -0500

    pleaase

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Selector from "./selector.jsx";
+import Selector from "./selectorPage.jsx";
 import "./index.css";
 
 const RouteWrapper = () => {
