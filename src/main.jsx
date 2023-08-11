736a1f8b1f4210e5b3170323525879d834d62e5d
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-08-10 22:32:30 -0500

    fixed import maybe

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Selector from "./selector.jsx";
 import "./index.css";
 
 const RouteWrapper = () => {
+  //uwu what's this
   const path = window.location.pathname.split("/");
 
   if (path.length > 1 && path[1] === "selector") {
